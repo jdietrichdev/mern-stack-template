9426ccba71bb54eb197639ae9ab4525db2ff28f4
jdietrichdev <jdietrich2016@gmail.com>
2020-05-04 18:36:50 -0500

    Updating Header for Register/Login links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -17,10 +17,14 @@ const Header = props => {
 
       <ul className='navbar-nav ml-auto'>
         {!props.user &&
-          <li className='nav-item'>
-            <Link to='/register'>Register</Link>
-            <Link to='/login'>Login</Link>
-          </li>
+          <>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/register'>Register</Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/login'>Login</Link>
+            </li>
+          </>
         }
         {props.user &&
           <li className="nav-item dropdown">
